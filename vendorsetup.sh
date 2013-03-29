71d154a4f6eb465518f22fc6d86eb88547ca20c0
LorD ClockaN <davor@losinj.com>
2013-03-29 16:19:15 +0100

    Remove unsuported devices from build environment

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,2 @@
 add_lunch_combo aokp_endeavoru-userdebug
-add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_mako-userdebug
-add_lunch_combo aokp_manta-userdebug
-add_lunch_combo aokp_toro-userdebug
-add_lunch_combo aokp_toroplus-userdebug
-add_lunch_combo aokp_grouper-userdebug
