13074adfea8aa385e8fe004571cd8ee381dd2134
C-RoMMix <mehmettayse@hotmail.com>
2013-03-29 22:24:13 +0100

    fix typo

--- products/galaxysmtd.mk
+++ products/galaxysmtd.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 # Inherit GSM common stuff
 $(call inherit-product, vendor/aokp/configs/gsm.mk)
 
-# Manta Overlay
+# galaxysmtd Overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/galaxysmtd
 
 # Setup device specific product configuration
@@ -19,7 +19,7 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9000 TARGET_DEVICE=GT-I9000 BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
 
-# Maguro specific packages
+# galaxysmtd specific packages
 PRODUCT_PACKAGES += \
     Thinkfree
 
