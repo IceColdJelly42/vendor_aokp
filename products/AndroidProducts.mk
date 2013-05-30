9580b66f9c35f322154db4408b0328c05645cfb2
LorD ClockaN <davor@losinj.com>
2013-05-30 12:19:49 +0200

    m7->m7ul

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/endeavoru.mk \
-    $(LOCAL_DIR)/m7.mk \
+    $(LOCAL_DIR)/m7ul.mk \
     $(LOCAL_DIR)/mako.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
--- products/m7ul.mk
+++ products/m7ul.mk
@@ -1,5 +1,5 @@
 # Inherit device configuration
-$(call inherit-product, device/htc/m7/device_m7.mk)
+$(call inherit-product, device/htc/m7ul/full_m7ul.mk)
 
 # Inherit AOKP common bits
 $(call inherit-product, vendor/aokp/configs/common.mk)
@@ -8,18 +8,18 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 $(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := m7
+PRODUCT_RELEASE_NAME := m7ul
 
 # Device naming
-PRODUCT_DEVICE := m7
-PRODUCT_NAME := icecold_m7
+PRODUCT_DEVICE := m7ul
+PRODUCT_NAME := icecold_m7ul
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := HTC One
 PRODUCT_MANUFACTURER := HTC
 
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7/m7:4.1.2/JZO54K/166937.7:user/release-keys" PRIVATE_BUILD_DESC="1.29.651.7 CL166937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="htc_europe/m7/m7:4.1.2/JZO54K/177101.12:user/release-keys" PRIVATE_BUILD_DESC="1.29.401.12 CL177101 release-keys"
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
